e8a6ae2f46c6b1a8e9ab2433da398f40b37a11fe
nexus-26 <30656935+nexus-26@users.noreply.github.com>
2017-09-05 15:14:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,8 +22,6 @@ button.onclick = function(){
    
 }; 
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn'); 
 submit.onclick = function(){
     //make a request to the server and send the names
@@ -48,6 +46,8 @@ submit.onclick = function(){
     };
     
     //Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://localhost/submit-name?name=' + name, true);
     request.send(null);
 };
