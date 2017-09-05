fcb262f820bea4cb6de651bf3f54d71bbc2cadd4
nexus-26 <30656935+nexus-26@users.noreply.github.com>
2017-09-05 14:01:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+button.onclick = function(){
+    //Create a request object
+    var request = new XMLHttpRequest();
+    //Capture the result
+    request.onreadystatechange = function(){
+      if(request.readystate === XMLHttpRequest.DONE){
+          if(request.status === 200){
+              var counter = request.responseText;
+              var span=document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      }  
+    };
+    //Make the request
+    request.open('GET', 'http://localhost/counter', true);
+    request.send(null);
+}; 
