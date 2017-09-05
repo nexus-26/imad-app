a00e5d2b3b0749a0de7c16b2b5296c525bd4f90f
nexus-26 <30656935+nexus-26@users.noreply.github.com>
2017-09-05 15:08:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,6 @@ app.get('/submit-name', function (req, res) {
     res.send(JSON.stringify(names));
 });
 
-
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
