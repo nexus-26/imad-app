d05c496778a3ef37344c388b7b88ca3961e4ecac
nexus-26 <30656935+nexus-26@users.noreply.github.com>
2017-09-05 15:20:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,6 +48,6 @@ submit.onclick = function(){
     //Make the request
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
-    request.open('GET', 'http://localhost/submit-name?name=' + name, true);
+    request.open('GET', 'http://adhikarlanitesh.imad.hasura-app.io//submit-name?name=' + name, true);
     request.send(null);
 };
